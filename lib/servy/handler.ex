7c962f962f503697e424826ac5346d791160a2a8
G. Zentkovich <gzentkovich@gmail.com>
2018-02-17 09:10:41 -1000

    revised routes, log, etc

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,12 +1,70 @@
 defmodule Servy.Handler do
   def handle(request) do
-    request 
+    request
     |> parse
     |> rewrite_path
-    |> log 
-    |> route 
+    |> log
+    |> route
     |> track
     |> format_response
+  end
+
+  def parse(request) do
+    [method, path, _] =
+      request
+      |> String.split("\n")
+      |> List.first
+      |> String.split(" ")
+
+    %{ method: method,
+       path: path,
+       resp_body: "",
+       status: nil
+     }
+  end
+
+  def rewrite_path(%{path: "/wildlife"} = conv) do
+    %{ conv | path: "/wildthings" }
+  end
+
+  # def rewrite_path(%{path: "/bears?id=" <> id} = conv) do
+  #   %{ conv | path: "/bears/#{id}" }
+  # end
+
+  def rewrite_path(%{path: path} = conv) do
+    regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
+    captures = Regex.named_captures(regex, path)
+    rewrite_path_captures(conv, captures)
+  end
+
+  def rewrite_path(conv), do: conv
+
+  def rewrite_path_captures(conv, %{"thing" => thing, "id" => id}) do
+    %{ conv | path: "/#{thing}/#{id}" }
+  end
+
+  def rewrite_path_captures(conv, nil), do: conv
+
+  def log(conv), do: IO.inspect conv
+
+  def route(%{ method: "GET", path: "/wildthings" } = conv) do
+    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
+  end
+
+  def route(%{ method: "GET", path: "/bears" } = conv) do
+    %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
+  end
+
+  def route(%{ method: "GET", path: "/bears/" <> id } = conv) do
+    %{ conv | status: 200, resp_body: "Bear #{id}" }
+  end
+
+  def route(%{ method: "GET", path: "/lions/" <> id } = conv) do
+    %{ conv | status: 200, resp_body: "Lion #{id}" }
+  end
+
+  def route(%{ path: path } = conv) do
+    %{ conv | status: 404, resp_body: "No #{path} here!"}
   end
 
   def track(%{status: 404, path: path} = conv) do
@@ -15,44 +73,6 @@ defmodule Servy.Handler do
   end
 
   def track(conv), do: conv
-
-  def rewrite_path(%{path: "/wildlife"} = conv) do
-    %{ conv | path: "/wildthings" }
-  end
-
-  def rewrite_path(conv), do: conv
-
-  def log(conv), do: IO.inspect conv
-
-  def parse(request) do
-    [method, path, _] =
-      request 
-      |> String.split("\n") 
-      |> List.first    
-      |> String.split(" ")
-
-    %{ method: method, 
-       path: path, 
-       resp_body: "",
-       status: nil
-     }
-  end
-
-  def route(%{ method: "GET", path: "/wildthings" } = conv) do
-    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }          
-  end
-
-  def route(%{ method: "GET", path: "/bears" } = conv) do
-    %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }    
-  end
-
-  def route(%{ method: "GET", path: "/bears/" <> id } = conv) do
-    %{ conv | status: 200, resp_body: "Bear #{id}" }
-  end
-
-  def route(%{ path: path } = conv) do
-    %{ conv | status: 404, resp_body: "No #{path} here!"}
-  end
 
   def format_response(conv) do
     """
@@ -126,7 +146,7 @@ response = Servy.Handler.handle(request)
 IO.puts response
 
 request = """
-GET /wildlife HTTP/1.1
+GET /bears?id=1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
@@ -137,3 +157,26 @@ response = Servy.Handler.handle(request)
 
 IO.puts response
 
+request = """
+GET /lions?id=1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /wildlife HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
