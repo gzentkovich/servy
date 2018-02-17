fb4d7d17cadc7041f357dbea8006e18aabee3696
G. Zentkovich <gzentkovich@gmail.com>
2018-02-17 10:19:39 -1000

    add route error handler

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -63,6 +63,25 @@ defmodule Servy.Handler do
     %{ conv | status: 200, resp_body: "Lion #{id}" }
   end
 
+  def route(%{ method: "GET", path: "/about"} = conv) do
+    Path.expand("../../pages", __DIR__)
+    |> Path.join("about.html")
+    |> File.read
+    |> handle_file(conv)
+  end
+
+  def handle_file({:ok, content}, conv) do
+    %{ conv | status: 200, resp_body: content }
+  end
+
+  def handle_file({:error, :enoent}, conv) do
+    %{ conv | status: 404, resp_body: "File not found!"}
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{ conv | status: 500, resp_body: "File error: #{reason}"}
+  end
+
   def route(%{ path: path } = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here!"}
   end
@@ -171,6 +190,18 @@ IO.puts response
 
 request = """
 GET /wildlife HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /about HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
